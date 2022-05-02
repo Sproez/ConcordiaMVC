05aac961ff1fdc7558c1f3ea56dd7cc7ed7bbab6
Sproez <56766899+Sproez@users.noreply.github.com>
2022-05-02 10:41:15 +0200

    Tests

--- ConcordiaTrelloClient/Dto/CardDto.cs
+++ ConcordiaTrelloClient/Dto/CardDto.cs
@@ -7,10 +7,6 @@ using System.Text.Json.Serialization;
 
 public class CardDto
 {
-    //private static readonly string HighPriorityLabelId = "625032fb182ca5704dde89f7";
-    //private static readonly string MediumPriorityLabelId = "62503330156b3d273dbf4bf7";
-    //private static readonly string LowPriorityLabelId = "625033198048561b7029c770";
-
     [JsonPropertyName("id")]
     public string Id { get; init; } = null!;
 
@@ -28,17 +24,5 @@ public class CardDto
 
     [JsonPropertyName("idLabels")]
     public List<string> LabelIds { get; set; } = null!;
-    /*
-    public Priority Priority
-    {
-        get {
-            if (LabelIds.Contains(HighPriorityLabelId)) return Priority.High;
-            if (LabelIds.Contains(MediumPriorityLabelId)) return Priority.Medium;
-            if (LabelIds.Contains(LowPriorityLabelId)) return Priority.Low;
-            return Priority.Default;
-        }
-    }
-    */
-
 
 }
---
+++ UnitTests/Automapper/AutomapperTest.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using AutoMapper;
+using ConcordiaLib.Domain;
+using ConcordiaLib.Enum;
+using ConcordiaTrelloClient.Dto;
+using ConcordiaTrelloClient.Dto.NestedProperties;
+using ConcordiaTrelloClient.Mapping;
+using ConcordiaTrelloClient.Options;
+using NUnit.Framework;
+
+namespace UnitTests.Automapper;
+
+public class AutomapperTest
+{
+
+    IMapper mapper = null!;
+
+    [SetUp]
+    public void Setup()
+    {
+        var options = new ApiOptions();
+        var pResolver = new PriorityResolver(options);
+        var automapperConfig = new MapperConfiguration(cfg =>
+        {
+            cfg.CreateMap<CardDto, Card>()
+                .ForCtorParam("Priority",
+                opt => opt.MapFrom(src => pResolver.Resolve(src.LabelIds)));
+            cfg.CreateMap<CardListDto, CardList>();
+            cfg.CreateMap<PersonDto, Person>();
+            cfg.CreateMap<CommentDto, Comment>();
+        });
+
+        mapper = automapperConfig.CreateMapper();
+    }
+
+    [Test]
+    public void PersonTest()
+    {
+        var entity = new Person("A") { Id = "B" };
+        var dto = new PersonDto() { Name = "A", Id = "B" };
+
+        var mappedEntity = mapper.Map<PersonDto, Person>(dto);
+
+        Assert.AreEqual(entity, mappedEntity);
+    }
+
+    [Test]
+    public void CardListTest()
+    {
+        var entity = new CardList("A") { Id = "B" };
+        var dto = new CardListDto() { Name = "A", Id = "B" };
+
+        var mappedEntity = mapper.Map<CardListDto, CardList>(dto);
+
+        Assert.AreEqual(entity, mappedEntity);
+    }
+
+    [Test]
+    public void CommentTest()
+    {
+        var entity = new Comment("A", new DateTime(2000, 1, 1)) { Id = "B", CardId = "C", PersonId = "D" };
+        var dto = new CommentDto()
+        {
+            Id = "B",
+            CreatedAt = new DateTime(2000, 1, 1),
+            PersonId = "D",
+            Data = new NestedData()
+            {
+                Text = "A",
+                Card = new NestedCard()
+                {
+                    Id = "C",
+                    AssigneesIds = new List<string>() { "E", "F", "G" }
+                }
+            }
+        };
+
+        var mappedEntity = mapper.Map<CommentDto, Comment>(dto);
+
+        Assert.AreEqual(entity, mappedEntity);
+    }
+
+    [Test]
+    public void CardTest()
+    {
+        var entity = new Card("A", "B", new DateTime(2000, 1, 1), Priority.High) { Id = "C", CardListId = "D" };
+        var dto = new CardDto()
+        {
+            Title = "A",
+            Description = "B",
+            DueBy = new DateTime(2000, 1, 1),
+            LabelIds = new List<string>() { "625032fb182ca5704dde89f7" },
+            Id = "C",
+            CardListId = "D"
+        };
+
+        var mappedEntity = mapper.Map<CardDto, Card>(dto);
+
+        Assert.AreEqual(entity, mappedEntity);
+    }
+
+    [Test]
+    public void AssignmentTest()
+    {
+        var entityA = new Assignment() { PersonId = "A", CardId = "Z" };
+        var entityB = new Assignment() { PersonId = "B", CardId = "Z" };
+        var entityC = new Assignment() { PersonId = "C", CardId = "Z" };
+        var dto = new NestedCard()
+        {
+            Id = "Z",
+            AssigneesIds = new List<string>() { "A", "B", "C" }
+        };
+
+        var expected = new HashSet<Assignment>() { entityA, entityB, entityC };
+        var result = new HashSet<Assignment>();
+
+        foreach (var aId in dto.AssigneesIds)
+        {
+            var assignment = new Assignment()
+            {
+                CardId = dto.Id,
+                PersonId = aId
+            };
+            result.Add(assignment);
+        }
+
+        Assert.AreEqual(expected, result);
+    }
+}
+
